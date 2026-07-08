c65aa94872067c6a74c60c11643b42e8798f39b4
HIBA <hiba49144@gmail.com>
2026-07-08 09:24:38 +0530

    Time: 22 ms (51.47%), Space: 54 MB (97.06%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (s.length < 2) return s;
+
+    let start = 0;
+    let maxLength = 1;
+
+    function expand(left, right) {
+        while (
+            left >= 0 &&
+            right < s.length &&
+            s[left] === s[right]
+        ) {
+            if (right - left + 1 > maxLength) {
+                maxLength = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        // Odd length palindrome
+        expand(i, i);
+
+        // Even length palindrome
+        expand(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLength);
+}
+console.log(longestPalindrome("babad"))
+console.log(longestPalindrome("cbbd"))
