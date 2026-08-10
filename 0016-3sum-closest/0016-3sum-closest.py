48a0f0d68f656f514bdcfe8b737805c981ec93bd
HIBA <hiba49144@gmail.com>
2026-08-10 08:16:30 +0530

    Time: 438 ms (87.32%), Space: 12.5 MB (42.77%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if abs(curr_sum - target) < abs(closest - target):
+                    closest = curr_sum
+
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+                else:
+                    return curr_sum
+
+        return closest
+        
+        
