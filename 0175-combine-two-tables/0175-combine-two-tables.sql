119ceca018e62cae2793dc2780875cdbbdb360b0
HIBA <hiba49144@gmail.com>
2026-09-18 10:06:37 +0530

    Time: 489 ms (35.1%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    Person.firstName,
+    Person.lastName,
+    Address.city,
+    Address.state
+FROM person
+LEFT JOIN address
+ON person.personId = address.personId;
